0a7914a8a7d78e4e1dc894d1d038dc6ddf347029
Waseem Idelbi <idelbiw@gmail.com>
2020-11-24 21:54:01 -0500

    Initial Commit

---
+++ CalorieTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Waseem Idelbi on 11/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
