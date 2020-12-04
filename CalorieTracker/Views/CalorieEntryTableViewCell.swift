477fdcd781175213967f22d3870315ac220e2ca5
Waseem Idelbi <idelbiw@gmail.com>
2020-12-03 23:42:45 -0500

    added all the logic in the calorie entry tableView cell so it knows how to configure its views when it receives a CalorieEntry type property

--- CalorieTracker/Views/CalorieEntryTableViewCell.swift
+++ CalorieTracker/Views/CalorieEntryTableViewCell.swift
@@ -9,6 +9,27 @@ import UIKit
 
 class CalorieEntryTableViewCell: UITableViewCell {
 
+    //MARK: - Main properties -
+    var calorieEntry: CalorieEntry? {
+        didSet {
+            updateViews()
+        }
+    }
+    var dateFormatter: DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        return dateFormatter
+    }
+    
+    //MARK: - Methods -
+    func updateViews() {
+        guard let entry = calorieEntry else { return }
+        textLabel?.text = String(entry.calories)
+        detailTextLabel?.text = dateFormatter.string(from: entry.date)
+    }
+    
+    //MARK: - IBOutlets -
     
 
-}
+} //End of class
