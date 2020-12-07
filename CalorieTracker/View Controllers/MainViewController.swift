ca3cba542f9f6d1d912224c0d0d4b8815302c290
Waseem Idelbi <idelbiw@gmail.com>
2020-12-07 14:19:27 -0500

    saving changes

---
+++ CalorieTracker/View Controllers/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Waseem Idelbi on 11/24/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    //MARK: - IBOutlets -
+    @IBOutlet weak var currentTotalLabel: UILabel!
+    @IBOutlet weak var calorieGoalLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - IBActions -
+    @IBAction func clearButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func addEntryButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+} //End of class
+
