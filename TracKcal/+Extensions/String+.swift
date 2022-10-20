62562689d350c35c8846d458261ebd5245b4b945
Waseem Idelbi <idelbiw@gmail.com>
2022-10-20 15:55:10 -0400

    Renamed project to TracKcal

---
+++ TracKcal/+Extensions/String+.swift
@@ -0,0 +1,15 @@
+//
+//  String+.swift
+//  CalorieTracker
+//
+//  Created by Waseem Idelbi on 12/1/20.
+//
+
+import Foundation
+
+extension String {
+    static let calorieEntryCell     = "CalorieEntryCell"
+    static let dailyGoalKey         = "DailyGoalKey"
+    static let entryDetailSegue     = "EntryDetailSegue"
+    static let optionCellIdentifier = "optionCellIdentifier"
+}
---
+++ TracKcal/+Extensions/UIView+.swift
@@ -0,0 +1,16 @@
+//
+//  UIButton+.swift
+//  CalorieTracker
+//
+//  Created by Waseem Idelbi on 3/5/21.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func circular() {
+        self.layer.cornerRadius = self.frame.height / 2
+    }
+    
+}
